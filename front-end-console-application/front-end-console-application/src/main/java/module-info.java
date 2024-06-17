71e1954ea37b92fda816130c58fb119caa785b1a
eleneskhulukhia <167057357+eleneskhulukhia@users.noreply.github.com>
2024-06-17 19:57:06 +0400

    Delete front-end-console-application/front-end-console-application directory

--- front-end-console-application/front-end-console-application/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.frontendconsoleapplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.frontendconsoleapplication to javafx.fxml;
-    exports org.example.frontendconsoleapplication;
-}
